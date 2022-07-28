47aae8556df4c9a4e52fefc35e83793db24a01d4
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2022-07-28 10:23:25 +0200

    add search

--- app/src/main/java/zw/co/nm/movies/activities/MainActivity.java
+++ app/src/main/java/zw/co/nm/movies/activities/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -40,14 +42,14 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         super.onCreate(savedInstanceState);
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
-        testOne();
+        testOne("");
 
     }
 
-    private void testOne() {
+    private void testOne(String query) {
         activityMainBinding.progBar.setVisibility(View.VISIBLE);
         movies = new ArrayList<>();
-        Call<GetMovieResponse> call = Retrofit.getService().getMovies();
+        Call<GetMovieResponse> call = Retrofit.getService().getMovies(query);
         call.enqueue(new Callback<GetMovieResponse>() {
             @Override
             public void onResponse(Call<GetMovieResponse> call, Response<GetMovieResponse> response) {
@@ -78,13 +80,27 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                testOne(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
 
         return true;
     }
 
     @Override
     public void onMovieItemClick(int position) {
-        startActivity(new Intent(this,MovieDetailActivity.class));
+        startActivity(new Intent(this, MovieDetailActivity.class));
 
     }
 }
--- app/src/main/java/zw/co/nm/movies/api/ApiService.java
+++ app/src/main/java/zw/co/nm/movies/api/ApiService.java
@@ -3,11 +3,15 @@ package zw.co.nm.movies.api;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 import zw.co.nm.movies.api.responses.GetMovieResponse;
 
 public interface ApiService {
 
     @GET("list_movies.json")
     Call<GetMovieResponse> getMovies();
+
+    @GET("list_movies.json")
+    Call<GetMovieResponse> getMovies(@Query("query_term") String query);
 
 }
