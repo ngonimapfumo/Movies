f999a61c48de4c4803d59700e48162ec1171acf1
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2022-08-17 09:06:24 +0200

    UI fix

---
+++ app/src/main/java/zw/co/nm/movies/activities/viewmodels/MainActivityViewModel.java
@@ -0,0 +1,14 @@
+package zw.co.nm.movies.activities.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+
+}
