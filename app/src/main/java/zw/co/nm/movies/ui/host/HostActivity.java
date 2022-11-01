f05253db8f200dbd13378bda7d9a3e2291b2a460
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2022-11-02 01:51:59 +0200

    add search to host

--- app/src/main/java/zw/co/nm/movies/ui/host/HostActivity.java
+++ app/src/main/java/zw/co/nm/movies/ui/host/HostActivity.java
@@ -3,9 +3,14 @@ package zw.co.nm.movies.ui.host;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import zw.co.nm.movies.R;
 import zw.co.nm.movies.databinding.ActivityHostBinding;
 import zw.co.nm.movies.utils.NetworkChangeListener;
 
@@ -33,5 +38,29 @@ public class HostActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         unregisterReceiver(networkChangeListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.onActionViewExpanded();
+        searchView.requestFocusFromTouch();
+        searchView.setQueryHint("Search Movies");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+               // testOne(s, 50);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return true;
     }
 }
